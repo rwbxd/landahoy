edbbd2253c5a2ebb9807002d8a935754dec57204
Will <will.bryan99@gmail.com>
2025-02-18 23:22:42 -0600

    Add start of client <-> server and server <-> sonarr communication

--- client/src/Series.tsx
+++ client/src/Series.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react"
+
 function Series() {
+  const [series, setSeries] = useState<SeriesAPIResponse[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5050").then(res => res.json()).then(data => {console.log(data); setSeries(data);})
+  }, [])
+
   return (
     <>
       <p>Content here</p>
+      <div>{series.map((s) => {
+        return (
+          <div key={s.id}>
+            <p>Series: {s.tvdb_id}</p>
+            <p>Complete: {s.complete.toString()}</p>
+          </div>
+        )
+      })}</div>
     </>
   )
+}
+
+interface SeriesAPIResponse {
+  id: string;
+  tvdb_id: number;
+  complete: boolean;
 }
 
 export default Series
--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express from "express";
 import pg from 'pg';
 const { Pool } = pg;
@@ -14,12 +15,23 @@ const app = express();
 
 app.get('/', async (_, res) => {
     const result = await db.query("SELECT * FROM landahoy;");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
     if (result.rowCount) {
         res.send(result.rows);
     } else {
         res.send("Didn't find any results");
     }
     res.end();
+})
+
+const apiURL = process.env.SONARR_URL + '/api/v3';
+app.get('/import', async (_, res) => {
+    console.log(process.env);
+    const sonarrResponse = await fetch(apiURL + '/series', {headers: [['X-Api-Key', process.env.SONARR_TOKEN as string]]});
+    const sonarrResponseData = await sonarrResponse.json();
+    res.contentType('application/json');
+    res.end(JSON.stringify(sonarrResponseData));
 })
 
 app.listen(PORT, () => {
